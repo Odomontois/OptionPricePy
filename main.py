d7403ebdc8bf570335ec90224fe81c3ec20dabee
Odomontois <odomontois@gmail.com>
2013-11-03 13:52:59 +0400

    added cached in levelCached decorators

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from rbt import Interval as Int, IntervalValue as Val, IntervalCache as Cache
+from intcache import Interval as Int, IntervalValue as Val, IntervalCache as Cache
 
 a = Int(3,5)
 b = Int(4,6)
@@ -13,4 +13,4 @@ m = Cache()
 
 m[4,5] = 3
 m[6,7] = 2
-print 5.5 in m, 4 in m, 6 in m,m[4],m[6.5]
+print 5.5 in m, 4 in m, 6 in m, m[5], m[6.5]
